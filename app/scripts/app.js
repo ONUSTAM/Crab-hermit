b401ca91c813740242ea3cadbf5dc08b4bafd7a6
unknown <takuma_matsuno@PC333.BigTreeCapital.local>
2016-05-11 19:39:57 +0900

    study: rxjs

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -22,14 +22,6 @@ $(function() {
     });
   });
 
-  var clickStream = Rx.Observable.fromEvent(document, "mouseup");
-  clickStream
-      .buffer(clickStream.throttle(250))
-      .map(function(x) {return x.length})
-      .filter(function(n) {return n >= 2})
-      .subscribe(function(n) {console.log(n + "click")});
-
-
   // 各画面によって出しわけする
   dispatcher(location.pathname);
 });
