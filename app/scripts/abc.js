b3a0b8f68a63c40d8768d739d31ca330cf7f174e
ONUSTAM <artckma@gmail.com>
2016-05-30 09:27:30 +0900

    recode

--- app/scripts/abc.js
+++ app/scripts/abc.js
@@ -1,9 +1,26 @@
-'use strict';
-function ClassE(){
-  //constructor
+/*eslint no-unused-vars: "error"*/
+/*global Person*/
+// function ClassE(){
+//   //constructor
+// }
+// (function(p){
+//   p.say = function(){
+//     return 'ClassE !!!';
+//   };
+// })(ClassE.prototype);
+class Person {
+  'use strict'
+
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  sayHello() {
+    console.log('Hello, Im ' + this.getName());
+  }
+
+  getName() {
+    return this.name;
+  }
 }
-(function(p){
-  p.say = function(){
-    return 'ClassE !!!';
-  };
-})(ClassE.prototype);
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,11 +1,13 @@
-/*global ClassE Conf:true*/
+/*global Person Conf:true*/
 /*eslint no-undef: "error"*/
 (function() {
   'use strict';
   console.log('app');
 
-  var classE = new ClassE();
-  console.log( classE.say() );
+  // var classE = new ClassE();
+  // console.log( classE.say() );
+  var person = new Person('tom', 11);
+  person.sayHello();
 
   var conf = new Conf();
   console.log(conf.viewPath);
